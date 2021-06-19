cc9918efb46622cc6c06b329a2278bc665a1cd7b
cccwz <2365503287@qq.com>
2021-06-19 21:53:19 +0800

    支付微服务集群配置

--- cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -4,9 +4,7 @@ import com.atguigu.springcloud.entity.CommonResult;
 import com.atguigu.springcloud.entity.Payment;
 import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.tomcat.util.security.Escape;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -20,15 +18,14 @@ public class PaymentController {
 
     @Value("${server.port}")
     private String serverPort;
-
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment){
         int result=paymentService.create(payment);
         log.info("插入结果"+result);
         if(result>0){
-            return new CommonResult(200,"插入成功,端口"+serverPort,result);
+            return new CommonResult(200,"插入成功,端口号"+serverPort,result);
         }else {
-            return new CommonResult(444,"插入失败,端口"+serverPort,null);
+            return new CommonResult(444,"插入失败",null);
         }
     }
 
@@ -37,9 +34,9 @@ public class PaymentController {
         Payment result=paymentService.getPaymentById(id);
         log.info("查询结果"+result);
         if(result!=null){
-            return new CommonResult(200,"查询成功,端口"+serverPort,result);
+            return new CommonResult(200,"查询成功,端口号"+serverPort,result);
         }else {
-            return new CommonResult(444,"没有对应记录,"+id+"端口"+serverPort,null);
+            return new CommonResult(444,"没有对应记录"+id,null);
         }
     }
 }
